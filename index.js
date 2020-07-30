b813d7a2de0c162b1cbb71a936954d8c3e336cc7
ganawa <ghana.afani@gmail.com>
2020-07-31 01:20:52 +0700

    add package nodemon

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+// import express from "express";
 
 const app = express();
 
 const port = 7000;
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
+});
+
+app.get("/", (_,res) => {
+    // res.send("Hello world");
+    res.json({
+        msg: "hello world",
+        name: "gana"
+    });
 });
