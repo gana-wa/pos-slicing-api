10901159210d505e9baf602cec0541ea7f4c2983
ganawa <ghana.afani@gmail.com>
2020-07-31 22:33:52 +0700

    nothing here just commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const logger = require("morgan");
 const mySQL = require("mysql");
+const { resolve } = require("path");
+const { rejects } = require("assert");
+const { query } = require("express");
 
 const app = express();
 
@@ -21,8 +24,8 @@ const db = mySQL.createConnection({
     database: "week4_api"
 });
 db.connect(errMsg => {
-    if(errMsg) throw errMsg;
-    console.log("Database connected")
+    if (errMsg) throw errMsg;
+    console.log("Database connected");
 })
 
 // app.get("/1", (_,res) => {
@@ -35,14 +38,61 @@ db.connect(errMsg => {
 
 const Router = express.Router();
 app.use(Router);
-Router.get("/", (_, res) => {
-    res.json({
-        msg: "hello world"
-    })
+// Router.get("/", (_, res) => {
+//     res.json({
+//         msg: "hello world"
+//     })
+// });
+// Router.post("/",(req,res) => {
+//     console.log(req.body);
+//     res.json({
+//         msg:"Body sudah diterima", 
+//     })
+// })
+const queryPromiseGet = () => {
+    return new Promise((resolve, rejects) => {
+        const queryString = "SELECT tb_product.product_id, tb_product.product_name, tb_category.category_name, tb_product.price FROM tb_product JOIN tb_category WHERE tb_product.category_id = tb_category.category_id";
+        db.query(queryString, (err, data) => {
+            if (!err) {
+                resolve(data);
+            } else {
+                rejects(err);
+            }
+        });
+    });
+};
+Router.get("/products", (_, res) => {
+    queryPromiseGet()
+        .then((data) => {
+            res.json(data)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
 });
-Router.post("/",(req,res) => {
-    console.log(req.body);
-    res.json({
-        msg:"Body sudah diterima", 
-    })
+
+const queryPromisePost = (body) => {
+    // const { product_name, price, category_id } = body;
+    // const queryString = `INSERT INTO tb_product SET product_name="${product_name}", price=${price}, category_id="${category_id}"`
+    const queryString = "INSERT INTO tb_product SET ?";
+    return new Promise((resolve, rejects) => {
+        db.query(queryString, [body], (err, data) => {
+            // db.query(queryString, (err, data) => {
+            if (!err) {
+                resolve(data);
+            } else {
+                rejects(err);
+            }
+        })
+    });
+};
+Router.post("/products", (req, res) => {
+    queryPromisePost(req.body)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+            // res.status(500).json(err);
+        })
 })
