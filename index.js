a17bcde64b8b954e005094ba9c0bfe5e77c4952b
ganawa <ghana.afani@gmail.com>
2020-08-01 19:42:54 +0700

    checkpoint restore

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const logger = require("morgan");
-// const path = require("path");
-
-const indexRouter = require('./src/Routes/index');
+const mySQL = require("mysql");
+const { resolve } = require("path");
+const { rejects } = require("assert");
+const { query } = require("express");
 
 const app = express();
 
@@ -13,9 +15,18 @@ app.listen(port, () => {
 });
 app.use(bodyParser.json()); //json
 app.use(bodyParser.urlencoded({ extended: false })) //x-www-form-urlencoded
-app.use(logger("dev")); //morgan
+app.use(logger("dev"));
 
-app.use('indexRouter');
+const db = mySQL.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "week4_api"
+});
+db.connect(errMsg => {
+    if (errMsg) throw errMsg;
+    console.log("Database connected");
+})
 
 // app.get("/1", (_,res) => {
 //     // res.send("Hello world");
@@ -24,3 +35,87 @@ app.use('indexRouter');
 //         name: "gana"
 //     });
 // });
+
+const Router = express.Router();
+app.use(Router);
+// Router.get("/", (_, res) => {
+//     res.json({
+//         msg: "hello world"
+//     })
+// });
+// Router.post("/",(req,res) => {
+//     console.log(req.body);
+//     res.json({
+//         msg:"Body sudah diterima", 
+//     })
+// })
+const queryPromiseGet = () => {
+    return new Promise((resolve, rejects) => {
+        const queryString = "SELECT tb_product.product_id, tb_product.product_name, tb_category.category_name, tb_product.price FROM tb_product JOIN tb_category ON tb_product.category_id = tb_category.category_id";
+        db.query(queryString, (err, data) => {
+            if (!err) {
+                resolve(data);
+            } else {
+                rejects(err);
+            }
+        });
+    });
+};
+Router.get("/products", (_, res) => {
+    queryPromiseGet()
+        .then((data) => {
+            res.json(data)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+});
+
+const queryPromisePost = (body) => {
+    // const { product_name, price, category_id } = body;
+    // const queryString = `INSERT INTO tb_product SET product_name="${product_name}", price=${price}, category_id="${category_id}"`
+    const queryString = "INSERT INTO tb_product SET ?";
+    return new Promise((resolve, rejects) => {
+        db.query(queryString, [body], (err, data) => {
+            // db.query(queryString, (err, data) => {
+            if (!err) {
+                resolve(data);
+            } else {
+                rejects(err);
+            }
+        })
+    });
+};
+Router.post("/products", (req, res) => {
+    queryPromisePost(req.body)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+            // res.status(500).json(err);
+        })
+})
+
+// ROUTER & QUERY
+const queryPromiseGetById = (id) => {
+    return new Promise((resolve, rejects) => {
+        const queryString = "SELECT tb_product.product_id, tb_product.product_name, tb_category.category_name, tb_product.price FROM tb_product JOIN tb_category ON tb_product.category_id = tb_category.category_id WHERE tb_product.product_id = ?";
+        db.query(queryString, [id], (err, data) => {
+            if (!err) {
+                resolve(data);
+            } else {
+                rejects(err);
+            }
+        });
+    });
+};
+Router.get("/products/:id", (req, res) => {
+    queryPromiseGetById(req.params.id)
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+})
