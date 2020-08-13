7887d139b61af9cb988ab21f0c55700fb1c799f1
ganawa <ghana.afani@gmail.com>
2020-08-13 15:10:31 +0700

    (change) config sql

--- src/Configs/dbMySql.js
+++ src/Configs/dbMySql.js
@@ -2,8 +2,8 @@ const mySQL = require("mysql");
 
 const db = mySQL.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
     multipleStatements: true
 });
